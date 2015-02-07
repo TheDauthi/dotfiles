0686871fa5983039d685197559a755c2711b13cb
Billy Conn <billyconn@leadify.net>
2015-02-07 04:34:46 -0600

    Slightly better history options

--- bashrc.d/05-history.bash
+++ bashrc.d/05-history.bash
@@ -1,12 +1,11 @@
 # Don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
 
-HISTCONTROL=ignoreboth
-
 # append to the history file, don't overwrite it
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
+HISTSIZE=10000
+HISTFILESIZE=20000
 
+HISTCONTROL="erasedups:ignoreboth"
