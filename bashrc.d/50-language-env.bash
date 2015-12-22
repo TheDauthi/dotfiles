1ac0ed0f30f7a18dd02da1340a85448b3037f132
Billy Conn <billyconn@leadify.net>
2015-12-22 17:37:17 -0600

    Updated language ENV scripts to work the same for each environment

---
+++ bashrc.d/50-language-env.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Sets up rbenv in our environment
+languages="rb pl py"
+
+for language in $languages
+do
+  virtualenv_path="$HOME/.${language}env"
+  if [ -d $virtualenv_path ];
+  then
+    pathprepend "$virtualenv_path/bin"
+    eval "$(${language}env init -)"
+  fi
+done
