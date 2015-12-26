feffa6c7b938cdc53a7420be6f7317363bcd82da
Billy Conn <billyconn@leadify.net>
2015-12-26 17:34:30 -0600

    Add new install scripts

---
+++ install.d/plenv.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/tokuhirom/plenv.git ~/.plenv
+git clone https://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/
---
+++ install.d/rbenv.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
