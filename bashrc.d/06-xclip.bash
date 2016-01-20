ad67598442d9aed3cd313016ff608d759fb1ca86
Billy Conn <billyconn@leadify.net>
2016-01-20 02:49:05 -0600

    pbcopy/pbpaste aliases for xclip

---
+++ bashrc.d/06-xclip.bash
@@ -0,0 +1,6 @@
+case $OSTYPE in
+  linux*)
+    XCLIP=$(command -v xclip)
+    [[ $XCLIP ]] && alias pbcopy="$XCLIP -selection clipboard" && alias pbpaste="$XCLIP -selection clipboard -o"
+    ;;   
+esac
