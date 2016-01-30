e5acc283586bc81ada8181f86f38035963c46390
Billy Conn <billyconn@leadify.net>
2016-01-30 17:55:19 -0600

    Updated docker aliases to be functions

--- bashrc.d/80-docker-aliases.bash
+++ bashrc.d/80-docker-aliases.bash
@@ -1,13 +1,44 @@
 # Add some docker specific aliases for cleaning up
-alias docker-clean="docker images --no-trunc | grep none | grep -v 'IMAGE'  | awk '{print \$3}' | xargs -r docker rmi"
-alias docker-remove-stopped="docker ps -a --no-trunc --filter 'status=exited' -q | xargs -r docker rm"
-alias docker-stop-all='docker stop $(docker ps -a -q)'
-alias docker-remove-all='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
-alias docker-last-image="docker images --no-trunc -q | head -n 1"
-alias docker-last-container="docker ps -l -q"
-alias docker-ip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+function docker-clean() {
+  docker images --no-trunc | grep none | grep -v 'IMAGE'  | awk '{print $3}' | xargs -r docker rmi
+}
+
+function docker-remote-stopped() {
+  docker ps -a --no-trunc --filter 'status=exited' -q | xargs -r docker rm
+}
+
+function docker-stop-all() {
+  docker stop $(docker ps -a -q)
+}
+
+function docker-remove-all() {
+  docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
+}
+
+function docker-last-image() {
+  docker images --no-trunc -q | head -n 1
+}
+
+function docker-last-container() {
+  docker ps -l -q
+}
+
+function docker-ip() {
+  docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"
+}
 
 function docker-enter {
   docker exec -i -t $1 /bin/bash
 }
+
+docker() {
+  if command -v "docker-$1" > /dev/null 2>&1; then
+    subcommand=$1
+    shift
+    docker-$subcommand "$@"
+  else
+    command docker "$@"
+  fi
+}
+
 complete -F __docker_containers_running docker-enter
