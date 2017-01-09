4c203758b12ba364c315dab25cf23021ab06809b
Billy Conn <billyconn@leadify.net>
2017-01-09 17:04:00 -0600

    Two more fixes to PROMPT

--- init/shellrc.d/90-prompt.zsh
+++ init/shellrc.d/90-prompt.zsh
@@ -1,2 +1,9 @@
-PROMPT="%{$fg_bold[green]%}%n@%m%{$reset_color%}:%{$fg_bold[blue]%}${PWD/#$HOME/~}%{$reset_color%}\$ "
+PROMPT='%{$fg_bold[green]%}%n@%m%{$reset_color%}:%{$fg_bold[blue]%}${PWD/#$HOME/~}%{$reset_color%}'
+
+if [[ $UID -eq 0 ]]; then
+    PROMPT="$PROMPT# "
+else
+    PROMPT="$PROMPT$ "
+fi
+
 RPROMPT='${return_status}$(git_prompt_info)$(git_prompt_status)%{$reset_color%}'
