be59119a0018d6ec51e300a5255bf82f23f22f30
Billy Conn <billyconn@leadify.net>
2017-05-23 07:16:24 -0500

    Added upto, cross-platform up script

--- init/shellrc.d/10-upto.sh
+++ init/shellrc.d/10-upto.sh
@@ -6,3 +6,15 @@ upto()
     local upto=$1
     cd "${PWD/\/$upto\/*//$upto}"
 }
+
+function up {
+    if [[ "$#" < 1 ]] ; then
+        cd ..
+    else
+        CDSTR=""
+        for i in {1..$1} ; do
+            CDSTR="../$CDSTR"
+        done
+        cd $CDSTR
+    fi
+}
