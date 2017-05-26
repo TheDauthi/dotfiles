68161475223db6d4a4c41c776d8d2c0a86060ab3
Billy Conn <thedauthi@gmail.com>
2017-05-26 00:13:25 -0500

    Removing pretty cat. Large files crash it badly.

--- init/shellrc.d/20-pretty-cat.bash
+++ init/shellrc.d/20-pretty-cat.bash
@@ -1,17 +1,7 @@
 
 if $(command -v pygmentize &> /dev/null) ; then
   # get the full paths to binaries
-  CAT_BIN=$(which cat)
   LESS_BIN=$(which less)
-
-  # pigmentize cat and less outputs
-  function cat()
-  {
-      for var;
-      do
-          pygmentize "$var" 2>/dev/null || "$CAT_BIN" "$var";
-      done
-  }
 
   function less()
   {
