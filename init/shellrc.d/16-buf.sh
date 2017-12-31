efd862d12f5baaf348c63c12c0822143ef229f66
Billy W Conn <TheDauthi@gmail.com>
2017-12-30 21:16:08 -0600

    Add simple backup file function

--- init/shellrc.d/16-buf.sh
+++ init/shellrc.d/16-buf.sh
@@ -49,17 +49,3 @@ BACKUP
       --ignore-failed-read "${base_filename}"
   fi
 }
-
-####
-# Creates a temp directory and moves object to it to get deleted on reboot
-function tmpit() 
-{
-  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
-tmpit
-Usage: tmpit file-or-directory
-
-Moves parameter to tmp directory to be deleted on reboot
-HELP
-return; fi  
-  mkdir -p /tmp/.trash && mv "$@" /tmp/.trash; 
-}
