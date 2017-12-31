4d8f6cd09429c721cc4f274d1c05327096eb02cf
Billy W Conn <TheDauthi@gmail.com>
2017-12-30 21:13:50 -0600

    Add all of the docker/docker-compose aliases

---
+++ init/shellrc.d/71-docker-compose-aliases.sh
@@ -0,0 +1,129 @@
+####
+# Docker-Compose alias
+# This alias also adds the additional commands defined below
+# On no match, it will pass the commands off to docker-compose itself
+dkc() {
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
+dkc
+Usage: dkc [docker-compose-commands ...]
+
+Shortcut for docker, adding new docker aliases as commands.
+HELP
+return; fi  
+  case "$1" in
+    "sh")
+      shift
+      dcsh "$@"
+      ;;
+    "ex")
+      shift
+      dcex "$@"
+      ;;
+    "fsi")
+      shift
+      dcfsi "$@"
+      ;;
+    "fse")
+      shift
+      dcfse "$@"
+      ;;
+    *)
+      docker-compose "$@"
+    ;;
+  esac
+}
+
+####
+# Docker Shell
+# Start a bash shell in the given container
+dcsh() {
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
+dcsh
+Usage: dcsh [service]
+
+Begins a bash shell in the given docker-compose service
+HELP
+return; fi
+  docker-compose exec "$@" bash
+}
+
+####
+# Docker Exec
+# Runs a given program in a docker container
+dcex() {
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
+dcex
+Usage: dcex [service] [command]
+
+Executes a command in the given docker-compose service
+HELP
+return; fi
+  docker-compose exec "$@"
+}
+
+###
+# Docker-compose file-system-inspect
+# Shows what the filesystem looks like for the given container
+dcfsi() {
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
+dcfsi
+Usage: dcfsi [service-name ...]
+
+Inspects the filesystem of the named service(s)
+HELP
+return; fi
+  
+  local CONTAINERS=$(docker-compose ps -q "$@")
+  
+  if [ "$?" -ne "0" ]; then
+    return
+  fi
+  
+  while read -r CONTAINER_ID; do
+    docker export "${CONTAINER_ID}" | tar tf -     
+  done <<< "${CONTAINERS}"
+}
+
+###
+# Docker-compose file-system-export
+# Exports the given container's filesystem
+dcfse() {
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
+dcfse
+Usage: dkfse [service-name ...]
+
+Exports the filesystem of the named service(s)
+HELP
+return; fi
+  local -a SERVICES
+  
+  local USERID=$(id -u)
+
+  local _RED='\033[0;31m'
+  local _NC='\033[0m'
+
+  if [ $# -eq 0 ]; then
+    SERVICE_NAMES=$(docker-compose config --services)
+    while read -r SERVICE; do
+      SERVICES=( "${SERVICES[@]}" "$SERVICE" )
+    done <<< "${SERVICE_NAMES}"
+
+    if [[ !${SERVICES[@]} ]]; then
+      echo -e "${_RED}No services found${_NC}"
+    fi
+    return
+  else
+    SERVICES=( "$@" )
+  fi
+
+  for SERVICE_NAME in ${SERVICES[@]}; do
+    CONTAINER_ID=$(docker-compose ps -q "${SERVICE_NAME}")
+    if [[ "$?" != "0" ]]; then
+      continue
+    fi
+    mkdir -p "${SERVICE_NAME}"
+    docker export "${CONTAINER_ID}" | tar xf - --owner="${USERID}" --directory="${SERVICE_NAME}"
+  done
+   
+}
+
--- init/shellrc.d/80-docker-aliases.sh
+++ init/shellrc.d/80-docker-aliases.sh
@@ -1,18 +1,31 @@
-alias dkc='docker-compose'
-
+####
+# Docker alias
+# This docker alias also adds the additional commands defined below
+# On no match, it will pass the commands off to docker itself
 dk() {
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
+dk
+Usage: dk [docker-commands ...]
+
+Shortcut for docker, adding new docker aliases as commands.
+HELP
+return; fi  
   case "$1" in
-    "e")
+    "sh")
       shift
-      dke "$@"
+      dksh "$@"
+      ;;
+    "ex")
+      shift
+      dkex "$@"
+      ;;
+    "fsi")
+      shift
+      dkfsu "$@"
       ;;
     "fse")
       shift
       dkfse "$@"
-      ;;
-    "fsi")
-      shift
-      dkfsi "$@"
       ;;
     *)
       docker "$@"
@@ -20,45 +33,76 @@ dk() {
   esac
 }
 
+####
+# Docker Shell
+# Start a bash shell in the given container
 dksh() {
-  docker exec -it $1 bash
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
+dksh
+Usage: dksh [container]
+
+Begins a bash shell in the given docker container
+HELP
+return; fi  
+  docker exec -it "$1" bash
 }
 
-dcsh() {
-  docker-compose exec "$@" bash
-}
-
+####
+# Docker Exec
+# Runs a given program in a docker container
 dkex() {
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
+dced
+Usage: dkex [container] [options] [command]
+
+Executes a command in the given docker container
+HELP
+return; fi
   docker exec -it "$@"
 }
 
-dcex() {
-  docker-compose exec "$@"
-}
 
 ###
 # Docker file-system-inspect
+# Shows what the filesystem looks like for the given container
 dkfsi() {
-  local ID=$1
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
+dkfsi
+Usage: dkfsi [container]
+
+Inspects the filesystem of the given container
+HELP
+return; fi
+
+  local CONTAINER_ID=$1
   
   if [[ -z "$1" ]]; then
-    ID=$(docker ps -lq)
+    CONTAINER_ID=$(docker ps -lq)
   fi
 
-  docker export "$ID" | tar tf - 
+  docker export "${CONTAINER_ID}" | tar tf - 
 }
 
 ###
 # Docker file-system-export
+# Exports the given container's filesystem
 dkfse() {
-  local IMAGE=$1
-  
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
+dkfse
+Usage: dkfse [ image ]
+
+Exports the filesystem of the given container
+HELP
+return; fi
+  local CONTAINER_ID=$1
+  local USERID=$(id -u)
+
   if [[ -z "$1" ]]; then
-    IMAGE=$(docker ps -lq)
+    CONTAINER_ID=$(docker ps -lq)
   fi
 
-  mkdir -p "$IMAGE" && docker export "$IMAGE" | tar xf - --owner="$ID" --directory="$IMAGE"
+  mkdir -p "${CONTAINER_ID}" && docker export "${CONTAINER_ID}" | tar xf - --owner="${USERID}" --directory="${CONTAINER_ID}"
 }
 
+alias dkl="docker ps -lq"
 
-alias dkl="docker ps -l -q"
