83f706830b8ec53ca5368d4524742301367e97d1
Billy W Conn <TheDauthi@gmail.com>
2017-12-30 21:19:15 -0600

    Minor fixes to cd-to-project

--- init/shellrc.d/12-cdp.sh
+++ init/shellrc.d/12-cdp.sh
@@ -1,7 +1,17 @@
-cdp() 
+####
+# Find parent project directory
+cdp()
 {
-  PT=$(pwd)
-  while [[ "$PT" != "" && ! -e "$PT/.git" ]]; do
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then cat <<HELP
+cdp
+Usage: cdp [ base ]
+
+Change to parent directory containing [ base ] (default .git)
+HELP
+return; fi
+  local CDPBASE=${1:-.git}
+  local PT=$(pwd)
+  while [[ "$PT" != "" && ! -e "$PT/$CDPBASE" ]]; do
     PT=${PT%/*}
   done
   
