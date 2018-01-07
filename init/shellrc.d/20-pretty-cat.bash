2f99bb6880a87ba0e06a1fa84fcf64f9aaed9a2f
Billy W Conn <TheDauthi@gmail.com>
2018-01-06 20:15:45 -0600

    I disabled this long ago, removing it now

--- init/shellrc.d/20-pretty-cat.bash
+++
@@ -1,9 +0,0 @@
-if $(command -v pygmentize &> /dev/null) ; then
-  # get the full paths to binaries
-  LESS_BIN=$(which less)
-
-  function less()
-  {
-      pygmentize -g $* | "$LESS_BIN" -R
-  }
-fi
