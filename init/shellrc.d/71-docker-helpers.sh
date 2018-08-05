5b33050c70d2dfe137aa67bf329372d42890886c
Billy W Conn <TheDauthi@gmail.com>
2018-08-05 12:23:05 -0500

    Initial version of 'docker-helpers' function

---
+++ init/shellrc.d/71-docker-helpers.sh
@@ -0,0 +1,107 @@
+###
+# Docker 'helpers'
+docker-helper() {
+  HELP=$(cat <<'HELP'
+docker-helper
+Usage: docker-helper [command]
+Commands:
+  volume         Work with docker volumes
+HELP
+)  
+  if [[ "$1" == "-h" || "$1" == "--help" || "$1" == "" ]]; then 
+    echo "${HELP}"
+    return
+  fi
+  
+  COMMANDNAME="$1"
+  shift
+
+
+  case "${COMMANDNAME}" in
+    volume)
+        _docker_helpers_volume "$@"
+        ;;
+    *)
+        echo "${HELP}"
+        return
+  esac
+}
+
+_docker_helpers_volume() {
+  HELP=$(cat <<'HELP'
+docker-helper volume
+Usage: docker-helper volume [command]
+
+Commands:
+  clone          Work with docker volumes
+  
+HELP
+)
+  if [[ "$1" == "-h" || "$1" == "--help" || "$1" == "" ]]; then 
+    echo "${HELP}"
+    return
+  fi
+
+  COMMANDNAME="$1"
+  shift
+
+
+  case "${COMMANDNAME}" in
+    clone)
+        _docker_helpers_volume_clone "$@"
+        ;;
+    *)
+        echo "${HELP}"
+        return
+  esac
+
+}
+
+_docker_helpers_volume_clone() {
+  HELP=$(cat <<'HELP'
+docker-helper volume clone
+Usage: docker-helper volume VOLUME1 VOLUME2
+
+Clones VOLUME1 into VOLUME2
+  
+HELP
+)
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then 
+    echo "${HELP}"
+    return
+  fi
+  
+  if [ "$1" = "" ]; then
+    echo "Please provide a source volume name"
+    return
+  fi
+
+  if [ "$2" = "" ]; then
+    echo "Please provide a destination volume name"
+    return
+  fi
+
+  docker volume inspect $1 > /dev/null 2>&1
+  if [ "$?" != "0" ]; then
+    echo "The source volume '$1' does not exist"
+    return
+  fi
+  
+  #Now check if the destination volume name does not yet exist
+  docker volume inspect $2 > /dev/null 2>&1
+  if [ "$?" = "0" ]; then
+    echo "The destination volume '$2' already exists"
+    return
+  fi
+
+  echo "Creating destination volume '$2' ..."
+  docker volume create --name "$2"
+  echo "Copying data from source volume '$1' to destination volume '$2' ..."
+  docker run --rm \
+           -i \
+           -t \
+           -v $1:/from \
+           -v $2:/to \
+           alpine ash -c "cd /from ; cp -av . /to"
+}
+
