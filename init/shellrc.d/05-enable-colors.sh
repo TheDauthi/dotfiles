200f0b1459af0ed9b3594a0975cb25442c410c0a
Billy W Conn <TheDauthi@gmail.com>
2018-10-04 19:26:52 -0500

    Test dircolors with command instead of type

--- init/shellrc.d/05-enable-colors.sh
+++ init/shellrc.d/05-enable-colors.sh
@@ -1,16 +1,17 @@
-if [ ! -z "`type dircolors`" ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-	
-	if ls --help 2>&1 | grep -q -- --color
-	then
-    	alias ls='ls --color=auto -F'
-	else
-    	alias ls='ls -FG'
-	fi
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
+if command -v dircolors >/dev/null 2>&1; then
+  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
 fi
+
+
+
+if ls --help 2>&1 | grep -q -- --color; then
+ 	alias ls='ls --color=auto -F'
+else
+ 	alias ls='ls -FG'
+fi
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
 export CLICOLOR=1
