a17d7238926a4ccc06db2f18f9fec143499339b8
Billy W Conn <TheDauthi@gmail.com>
2018-11-02 23:30:03 -0500

    Use command -v instead of type

--- .installer/install.d/oh-my-zsh.sh
+++ .installer/install.d/oh-my-zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if type "zsh" > /dev/null; then
+if command -v  zsh > /dev/null; then
   if [ ! -d ~/.shellrc/oh-my-zsh ]; then
     git clone -q https://github.com/robbyrussell/oh-my-zsh ~/.shellrc/oh-my-zsh
   fi
