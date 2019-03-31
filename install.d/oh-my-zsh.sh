b00f86647c9d48e4619e5d6c8435174c88fcc979
Billy W Conn <TheDauthi@gmail.com>
2019-03-31 16:54:56 -0500

    spacing changes

--- install.d/oh-my-zsh.sh
+++ install.d/oh-my-zsh.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-if command -v  zsh > /dev/null; then
+if command -v zsh >/dev/null; then
   if [ ! -d ~/.shellrc/oh-my-zsh ]; then
     git clone -q https://github.com/robbyrussell/oh-my-zsh ~/.shellrc/oh-my-zsh
   fi
 fi
-
