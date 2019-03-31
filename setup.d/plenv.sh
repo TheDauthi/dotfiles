1a87d06951c80d214959bba998736fe4e16c2fef
Billy W Conn <TheDauthi@gmail.com>
2019-03-31 16:53:44 -0500

    Modernized env installs

--- setup.d/plenv.sh
+++ setup.d/plenv.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
+
 git clone https://github.com/tokuhirom/plenv.git ~/.plenv
-git clone https://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/
+source ~/.shellrc/shellrc.d/*language-env.sh
+
+git clone https://github.com/tokuhirom/Perl-Build.git "$(plenv root)/plugins/perl-build/"
+git clone https://github.com/hfm/plenv-update.git "$(plenv root)/plugins/plenv-update/"
 
 exec "${SHELL}"
--- setup.d/rbenv.sh
+++ setup.d/rbenv.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env sh
+
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-git clone https://github.com/rbenv/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
-git clone https://github.com/rkh/rbenv-update.git ~/.rbenv/plugins/rbenv-update
+
+source ~/.shellrc/shellrc.d/*language-env.sh
+
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)/plugins/ruby-build"
+git clone https://github.com/rbenv/rbenv-vars.git "$(rbenv root)/plugins/rbenv-vars"
+git clone https://github.com/rbenv/rbenv-default-gems.git "$(rbenv root)/plugins/rbenv-default-gems"
+git clone https://github.com/rkh/rbenv-update.git "$(rbenv root)/plugins/rbenv-update"
+
+echo "bundler" >>"$(rbenv root)/default-gems"
+echo "pry" >>"$(rbenv root)/default-gems"
+echo "byebug" >>"$(rbenv root)/default-gems"
 
 exec "${SHELL}"
