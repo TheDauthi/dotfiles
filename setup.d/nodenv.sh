40422f6f7802705ab3cb5d7a9413db02150f5c23
Billy W Conn <TheDauthi@gmail.com>
2022-01-19 17:40:02 -0600

    fix for missing default-packages nodenv file

--- setup.d/nodenv.sh
+++ setup.d/nodenv.sh
@@ -12,4 +12,5 @@ git clone https://github.com/nodenv/nodenv-default-packages.git "$(nodenv root)/
 git clone https://github.com/nodenv/node-build.git "$(nodenv root)/plugins/node-build"
 git clone https://github.com/nodenv/nodenv-package-rehash.git "$(nodenv root)/plugins/nodenv-package-rehash"
 git clone https://github.com/nodenv/nodenv-vars.git "$(nodenv root)/plugins/nodenv-vars"
+touch "$(nodenv root)/default-packages"
 exec $SHELL
