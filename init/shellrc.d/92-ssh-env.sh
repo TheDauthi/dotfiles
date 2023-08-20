1ffbe9725deff24f07cd1b31d455754eef6a9f15
Billy W Conn <TheDauthi@gmail.com>
2023-08-20 10:20:53 -0500

    Fix generation of remote prompts

--- init/shellrc.d/92-ssh-env.sh
+++ init/shellrc.d/92-ssh-env.sh
@@ -3,6 +3,10 @@
 
 function build_prompt()
 {
+  if [ ! -z "ZSH_NAME" ] && [ -z "$fg_bold" ]; then
+    autoload -U colors && colors
+  fi
+
   local bash_prompt=\''[\\033[01;32m\]\\u@\\h\\[\\033[00m\\]:\\[\\033[01;34m\]\w\[\\033[00m\]\\$ '\'
   local zsh_prompt='%{$fg_bold[green]%}%n@%m%{$reset_color%}:%{$fg_bold[blue]%}${PWD/#$HOME/~}%{$reset_color%}'
   local LOAD_PROMPT_CMD="[ ! -z \$BASH ] && PS1=${bash_prompt}"
